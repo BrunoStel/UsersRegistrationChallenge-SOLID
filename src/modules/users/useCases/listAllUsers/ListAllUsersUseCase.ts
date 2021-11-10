4e293ed755724e3f6c6648805d881f991ec85fff
Bruno Stelmastchuk <brunostel@gmail.com>
2021-11-10 17:09:15 -0300

    Adding ListAllUsersController, UseCase, Route

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,26 @@
+import { validate } from "uuid";
+
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
-
-interface IRequest {
-  user_id: string;
-}
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  execute({ user_id }): User[] {
+    if (!validate(user_id)) {
+      throw new Error("ID is not valid");
+    }
+    const user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error("ID not found");
+    }
+    const isUserAdmin = user.admin;
+    if (isUserAdmin === false) {
+      throw new Error("User isn't an ADMIN");
+    }
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
