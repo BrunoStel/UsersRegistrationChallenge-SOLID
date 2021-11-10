a17a1d5f641dda94b6d8a39870ad28b4117f8d57
Bruno Stelmastchuk <brunostel@gmail.com>
2021-11-10 15:03:47 -0300

    CreateUser UseCase, Controller and Route

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -1,12 +1,20 @@
-import { Response, Request } from "express";
+/* eslint-disable prettier/prettier */
+import { Request, Response } from "express";
 
 import { CreateUserUseCase } from "./CreateUserUseCase";
+
 
 class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+     const {name, email } = request.body
+      const user = this.createUserUseCase.execute({ email, name });
+      return response.status(201).json(user);
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
   }
 }
 
